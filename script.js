c547806b26d4f36888b1adc99ef4045e21b55c7e
JINXIN CHEN <166711124+jinxin4869@users.noreply.github.com>
2025-01-26 11:55:10 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-    const stars = document.getElementById("stars")
-    const x = e.clientX / window.innerWidth
-    const y = e.clientY / window.innerHeight
-
-    stars.style.transform = `translate(${x * 50}px, ${y * 50}px)`
-})
-
